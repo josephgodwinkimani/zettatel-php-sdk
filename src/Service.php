3a6e28e983e425084fb1afc83b86061f4b2fd135
josephgodwinkimani <josephgodwinkimani@users.noreply.github.com>
2021-12-22 17:43:34 +0000

    Fix styling

--- src/Service.php
+++ src/Service.php
@@ -1,39 +1,41 @@
 <?php
+
 namespace Gkimani\ZettatelPhpSdk;
 
-abstract class Service 
+abstract class Service
 {
-	protected $client;
+    protected $client;
 
-	protected $userId;
+    protected $userId;
 
     protected $password;
 
-	protected $apiKey;
+    protected $apiKey;
 
     protected $senderid;
 
-	public function __construct($client, $userId, $password, $apiKey, $senderid)
-	{
-		$this->client 	= $client;
-		$this->userId = $userId;
-		$this->apiKey 	= $apiKey;
-        $this->password 	= $password;
-        $this->senderid 	= $senderid;
-	}
+    public function __construct($client, $userId, $password, $apiKey, $senderid)
+    {
+        $this->client = $client;
+        $this->userId = $userId;
+        $this->apiKey = $apiKey;
+        $this->password = $password;
+        $this->senderid = $senderid;
+    }
 
-	protected static function error($data) {
-		return [
-			'status' 	=> 'error',
-			'data'		=> $data
-		];
-	}
+    protected static function error($data)
+    {
+        return [
+            'status' => 'error',
+            'data' => $data,
+        ];
+    }
 
-
-	protected static function success($data) {
-		return [
-			'status' 	=> 'success',
-			'data'		=> json_decode($data->getBody()->getContents())
-		];
-	}
+    protected static function success($data)
+    {
+        return [
+            'status' => 'success',
+            'data' => json_decode($data->getBody()->getContents()),
+        ];
+    }
 }
